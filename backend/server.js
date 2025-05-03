06a4d09a693290af05474648bf70938850463931
Mouli-js <moulieswaran30417@gmail.com>
2025-05-04 00:44:17 +0530

    Final Commit

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-if(process.nextTick.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') 
+    {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
     app.get("*", (req, res)=>{
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
